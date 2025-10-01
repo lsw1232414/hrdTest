610b092c86f488cafd9621fd5d1b2c916b9ece02
lsw1232414 <limsw980821@gmail.com>
2025-10-01 14:12:18 +0900

    artist

---
+++ ex-artist/src/com/lsw/DBMain.java
@@ -0,0 +1,12 @@
+package com.lsw;
+
+import com.lsw.crud.CrudClass;
+
+public class DBMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CrudClass cRUDClass = new CrudClass();
+	}
+
+}
